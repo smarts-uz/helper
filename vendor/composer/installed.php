27485169318f5de7a5c86de47e94af8f88c11a94
iBekzod <erkinovbegzod.45@gmail.com>
2021-04-05 19:38:38 +0500

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.1',
+    'version' => '1.0.1.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'teamprodev/helper',
+  ),
+  'versions' => 
+  array (
+    'teamprodev/helper' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
